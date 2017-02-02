e45d3cd144dd60eeae98c46ea79f748b7ae6b56e
flexairconsultant <flexairconsultant@gmail.com>
2017-02-02 10:40:31 -0600

    updated the bat file to exclude the 'saves' folder and not the 'saved' folder.

--- nogl.bat
+++ nogl.bat
@@ -1,1 +1,1 @@
-forfiles /p %APPDATA%\.minecraft /c "cmd /c if @isdir equ TRUE if not @file equ \"saved\" rmdir @file"
+forfiles /p %APPDATA%\.minecraft /c "cmd /c if @isdir equ TRUE if not @file equ \"saves\" rmdir @file"
